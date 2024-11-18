66d3466394346b496225999aa4d145f7e83b023e
Alvaro <alvaro.zabala@hotmail.es>
2024-11-18 17:38:36 +0100

    ZonaFit app Web con html jainfaces y primefaces

--- src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -17,7 +17,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
 
-@Component
+//@Component
 public class ZonaFitForma extends JFrame{
 
     private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
--- src/main/java/gm/zona_fit/repositorio/IClienteRepositorio.java
+++ src/main/java/gm/zona_fit/repositorio/IClienteRepositorio.java
@@ -3,4 +3,5 @@ package gm.zona_fit.repositorio;
 import gm.zona_fit.modelo.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// El integer es la clave primaria del objeto
 public interface IClienteRepositorio extends JpaRepository<Cliente,Integer> {}
