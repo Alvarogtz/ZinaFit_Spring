c085355f7e7778b4a6bba4bd4646951ea42e2644
Alvaro <alvaro.zabala@hotmail.es>
2024-11-18 20:32:33 +0100

    ZonaFit app Web con Spring JPA Joinfaces Primefaces

--- src/main/java/gm/zona_fit/controlador/IndexControlador.java
+++ src/main/java/gm/zona_fit/controlador/IndexControlador.java
@@ -3,14 +3,15 @@ package gm.zona_fit.controlador;
 import gm.zona_fit.modelo.Cliente;
 import gm.zona_fit.servicios.IClienteServicio;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -38,5 +39,37 @@ public class IndexControlador {
 
     public void agregarCliente(){
         this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente(){
+        logger.info("Cliente a guardar: " + this.clienteSeleccionado);
+
+        if(this.clienteSeleccionado.getId() == null){
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Cliente Agregado!"));
+        }else{
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Cliente Actualizado!"));
+        }
+
+        //Ocultar la ventana modal
+        PrimeFaces.current().executeInitScript("PF('ventanaModalCliente').hide()");
+        //Actualizamos la tabla
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes","forma-clientes:clientes-tabla");
+        // Reset del objeto seleccionado
+        this.clienteSeleccionado = null;
+    }
+
+    public void eliminarCliente(){
+        clienteServicio.eliminarCliente(this.clienteSeleccionado);
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Cliente Eliminado!"));
+        this.clientes.remove(this.clienteSeleccionado);
+        this.clienteSeleccionado = null;
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes","forma-clientes:clientes-tabla");
+
     }
 }
